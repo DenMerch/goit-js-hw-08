c009c8cbe00001eeffa752f1c29469be104069d7
DenMerch <rosignolli@gmail.com>
2023-01-14 18:21:52 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,26 @@
 import Player from "@vimeo/player";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+import { save, load } from "./localStorage.js";
+//додати лодаш
 
-player.on('timeupdate', function () {
-    player.getCurrentTime().then(function (seconds) {
-        localStorage.setItem("currentTime", JSON.stringify(seconds));
-        console.log(localStorage.getItem("currentTime"));
-    }).catch(function (error) {
-        console.error(error.message);
-    });
-});
+// player.on('timeupdate', function () {
+
+//     player.getCurrentTime().then(function (seconds) {
+//         save("videoplayer-current-time", JSON.stringify(seconds));
+//     }).catch(function (error) {
+//         console.error(error.message);
+//     });
+// });
+player.on("timeupdate", throttle(function (data) {
+    const time = data.seconds;
+    console.log(time);
+    save("videoplayer-current-time", JSON.stringify(time));
+    // save("videoplayer-current-time", time)
+}, 1000))
 player.on('play', function () {
-    player.setCurrentTime(localStorage.getItem("currentTime")).then(function (seconds) {
+    player.setCurrentTime(load("videoplayer-current-time")).then(function (seconds) {
         // seconds = the actual time that the player seeked to
     }).catch(function (error) {
         switch (error.name) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import { save, load } from "./localStorage.js";
+const formEl = document.querySelector("form");
+formEl.addEventListener("submit", storeData);
+function storeData(evt) {
 
+}
+console.log(3);
