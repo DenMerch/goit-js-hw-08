c130a65798dfa86ddefa4f028b939e5a5ca1d73c
DenMerch <rosignolli@gmail.com>
2023-01-14 19:46:43 +0200

    last

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,20 @@
 import { save, load } from "./localStorage.js";
+const throttle = require('lodash.throttle');
 const formEl = document.querySelector("form");
-formEl.addEventListener("submit", storeData);
-function storeData(evt) {
-
+formEl.addEventListener("submit", throttle(handleSubmit, 1000));
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, message }
+    } = event.currentTarget;
+    if (email.value === "" || message.value === "") {
+        return console.log("Please fill in all the fields!");
+    }
+    save("feedback-form-state", { email: email.value, message: message.value })
+    event.currentTarget.reset();
 }
-console.log(3);
+if (load("feedback-form-state")) {
+    formEl.email.value = load("feedback-form-state").email;
+    formEl.message.value = load("feedback-form-state").message;
+
+} else { return }
