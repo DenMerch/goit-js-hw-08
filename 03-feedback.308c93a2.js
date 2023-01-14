2f2e3d7e7551bd75c2a01599e74721998c2524ad
DenMerch <111914774+DenMerch@users.noreply.github.com>
2023-01-14 16:22:21 +0000

    Deploying to gh-pages from @ DenMerch/goit-js-hw-08@c009c8cbe00001eeffa752f1c29469be104069d7 🚀

---
+++ 03-feedback.308c93a2.js
@@ -0,0 +1,2 @@
+document.querySelector("form").addEventListener("submit",(function(e){})),console.log(3);
+//# sourceMappingURL=03-feedback.308c93a2.js.map
---
+++ 03-feedback.5346bb8a.js
@@ -0,0 +1,2 @@
+document.querySelector("form").addEventListener("submit",(function(e){})),console.log(3);
+//# sourceMappingURL=03-feedback.5346bb8a.js.map
